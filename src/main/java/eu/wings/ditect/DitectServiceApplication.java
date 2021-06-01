f7c682d8ccc983ab9fd8f2e1ddc094408ff8e05d
Manousos Mathioudakis <mmathioudakis@wings-ict-solutions.eu>
2021-06-01 16:50:52 +0300

    initial commit

---
+++ src/main/java/eu/wings/ditect/DitectServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.wings.ditect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DitectServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/wings/ditect/DitectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.wings.ditect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
