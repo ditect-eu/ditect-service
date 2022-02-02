55b63079323fe3d00801819336f3abc38b55aa88
thendriks <t.hendriks@maastrichtuniversity.nl>
2022-02-02 15:54:49 +0100

    Reverting changes for now to standard JSON until we have a converter to translate between both.

--- src/main/java/eu/ditect/domain/MetricMetaRequest.java
+++ src/main/java/eu/ditect/domain/MetricMetaRequest.java
@@ -9,26 +9,15 @@ import lombok.Data;
  */
 @Data
 public final class MetricMetaRequest {
-  @JsonProperty("ditect:partnerCode")
   private String partner;
-  @JsonProperty("ditect:pilotCode")
   private String pilotCode;
-  @JsonProperty("schema:country")
   private String country;
-  @JsonProperty("schema:region")
   private String region;
-  @JsonProperty("ditect:manufacturingProcessing")
   private boolean manufacturingProcessing;
-  @JsonProperty("ditect:primaryProduction")
   private boolean primaryProduction;
-  @JsonProperty("ditect:distributionRetail")
   private boolean distributionRetail;
-  @JsonProperty("ditect:packingStage")
   private boolean packingStage;
-  @JsonProperty("ditect:instrumentName")
   private String instrumentName;
-  @JsonProperty("ditect:typeOfAnalysis")
   private String typeOfAnalysis;
-  @JsonProperty("schema:dateCreated")
   private Instant createdDate;
 }
--- src/main/java/eu/ditect/domain/projection/MetricWithoutFileData.java
+++ src/main/java/eu/ditect/domain/projection/MetricWithoutFileData.java
@@ -1,7 +1,6 @@
 package eu.ditect.domain.projection;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.ditect.domain.Region;
 
 /**
@@ -11,36 +10,25 @@ public interface MetricWithoutFileData {
 
   String getId();
 
-  @JsonProperty("ditect:partnerCode")
   String getPartner();
 
-  @JsonProperty("ditect:pilotCode")
   String getPilotCode();
 
-  @JsonProperty("schema:region")
   Region getRegion();
 
-  @JsonProperty("schema:country")
   String getCountry();
 
-  @JsonProperty("ditect:manufacturingProcessing")
   boolean getManufacturingProcessing();
 
-  @JsonProperty("ditect:primaryProduction")
   boolean getPrimaryProduction();
 
-  @JsonProperty("ditect:distributionRetail")
   boolean getDistributionRetail();
 
-  @JsonProperty("ditect:packingStage")
   boolean getPackingStage();
 
-  @JsonProperty("ditect:instrumentName")
   String getInstrumentName();
 
-  @JsonProperty("ditect:typeOfAnalysis")
   String getTypeOfAnalysis();
 
-  @JsonProperty("schema:dateCreated")
   String getFileName();
 }
